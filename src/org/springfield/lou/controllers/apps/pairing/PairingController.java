03008eb5c1a449fe59a2933096313064ac6fabcc
Daniel Ockeloen <daniel@noterik.nl>
2016-10-15 12:02:21 +0200

    extra work on pairing

--- src/org/springfield/lou/controllers/apps/pairing/PairingController.java
+++ src/org/springfield/lou/controllers/apps/pairing/PairingController.java
@@ -20,6 +20,8 @@
 */
 package org.springfield.lou.controllers.apps.pairing;
 
+import java.util.Random;
+
 import javax.jws.WebParam.Mode;
 
 import org.json.simple.JSONObject;
@@ -47,36 +49,60 @@ public class PairingController extends Html5Controller {
 	public void attach(String sel) {
 		String selector = sel;
 		String hid = model.getProperty("@pairingid");
-		System.out.println("COOKIE HID="+hid);
 		if (hid!=null && !hid.equals("")) {
 			FsNode hidnode = model.getNode("/domain/mupop/config/hids/hid/"+hid);
-			if (hidnode!=null) {
-				System.out.println("HID="+hidnode.asXML());
-					model.setProperty("/screen/exhibitionpath","/domain/mupop/user/"+hidnode.getProperty("username")+"/exhibition/"+hidnode.getProperty("exhibitionid")+"/station/"+hidnode.getProperty("stationid"));
-					model.setProperty("/screen/sharedspace", "/shared/test"); // what does this do should be removed
-					screen.get("#screen").append("div", "exhibition",new ExhibitionController());
+			if (hidnode!=null) {		
+					String un = hidnode.getProperty("username");
+					String ei = hidnode.getProperty("exhibitionid");
+					String si = hidnode.getProperty("stationid");
+					if (!un.equals("") && !ei.equals("") && !si.equals("")) {
+						model.setProperty("/screen/exhibitionpath","/domain/mupop/user/"+un+"/exhibition/"+ei+"/station/"+si);
+						model.setProperty("/screen/sharedspace", "/shared/test"); // what does this do should be removed
+						screen.get("#screen").append("div", "exhibition",new ExhibitionController());
+					} else {
+						JSONObject data = new JSONObject();
+						data.put("unassignedmode","true");
+						data.put("hid",hidnode.getId());
+						screen.get(selector).render(data);	
+					}
+					model.onNotify("/shared['mupop']/hid['"+hid+"']","onDashboardMessage",this);
 			} else {
+				JSONObject data = new JSONObject();
+				data.put("nohidnode","true");
+				data.put("hid",hid);
+				screen.get(selector).render(data);	
 				System.out.println("NO HID NODE FOUND !");
 			}
-			JSONObject data = new JSONObject();
-			data.put("hid",hid);
-			screen.get(selector).render(data);
 		} else {
 			JSONObject data = new JSONObject();
 			data.put("pairingmode","true");
-			code = "HCES";
+			code = getRandomCode();
 			data.put("pairingcode",code);
 			screen.get(selector).render(data);	
-			System.out.println("SET HID REQUEST="+code);
 			model.onPropertyUpdate("/shared/mupop/hidresponse"+code,"onHidResponse",this);
 			model.setProperty("/shared/mupop/hidrequest",code);
 		}
 	}
 	
+	public void onDashboardMessage(ModelEvent e) {
+//		System.out.println("DASHBOARDMESSAGE="+e.getTargetFsNode().asXML());
+		screen.get("#screen").location("");
+	}
+	
 	public void onHidResponse(ModelEvent e) {
 		FsNode node = e.getTargetFsNode();
 		String hid = node.getProperty("hidresponse"+code);
-		System.out.println("SETTING HID="+hid);
 		model.setProperty("@pairingid",hid);
+	}
+	
+	private String getRandomCode() {
+		String options = "0123456789ABCDE";
+	    int len = options.length();
+	    Random r = new Random();
+	    String result = "";
+	    for (int i = 0; i < 4; i++) {
+	       result+=options.charAt(r.nextInt(len));
+	    }
+	    return result;
 	}
 }
