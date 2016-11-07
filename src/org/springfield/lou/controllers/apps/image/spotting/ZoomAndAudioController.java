dc969abc330c903ad2074d7154f73223d78a9bd3
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2016-11-07 17:02:56 +0100

    Temp url update

--- src/org/springfield/lou/controllers/apps/image/spotting/ZoomAndAudioController.java
+++ src/org/springfield/lou/controllers/apps/image/spotting/ZoomAndAudioController.java
@@ -21,9 +21,6 @@
 package org.springfield.lou.controllers.apps.image.spotting;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,7 +37,6 @@ import org.springfield.fs.FsNode;
 import org.springfield.fs.FsPropertySet;
 import org.springfield.lou.controllers.Html5Controller;
 import org.springfield.lou.model.ModelEvent;
-import org.springfield.lou.screen.Screen;
 
 /**
  * ZoomAndAudioController.java
@@ -51,154 +47,154 @@ import org.springfield.lou.screen.Screen;
  * 
  */
 public class ZoomAndAudioController extends Html5Controller {
-	
- 	private Map<String,  BufferedImage> images = new HashMap<String, BufferedImage>();
+
+    private Map<String, HashMap<String, Double>> spots = new HashMap<String, HashMap<String, Double>>();
+    private Map<String, BufferedImage> images = new HashMap<String, BufferedImage>();
     List<FsNode> nodes;
-    FsNode selecteditem = null;
+    private Map<String, FsNode> selecteditems = new HashMap<String, FsNode>();
 
-	public ZoomAndAudioController() {
+    public ZoomAndAudioController() {}
+
+    public void attach(String sel) {
+	selector = sel;
 		
-	}
-	
-	/*
-	public ZoomAndAudioController(String item) {
-		System.out.println("Loading item "+item);
-	}
-	*/
-	
-	public void attach(String sel) {
-		selector = sel;
+	String path = model.getProperty("/screen/exhibitionpath");
 		
-		String path = model.getProperty("/screen/exhibitionpath");
-		
-		FsNode stationnode = model.getNode(path);
-		if (stationnode!=null) {
-			FSList fslist = FSListManager.get("/domain/mecanex/app/sceneplayer/scene/blue/element/screen5/sounds",false);
-			nodes = fslist.getNodes();
-			loadImages();
-			JSONObject data = FSList.ArrayToJSONObject(nodes,screen.getLanguageCode(),"mask,url"); 
+	FsNode stationnode = model.getNode(path);
+	if (stationnode!=null) {
+	    FSList fslist = FSListManager.get("/domain/mecanex/app/sceneplayer/scene/blue/element/screen5/sounds",false);
+	    nodes = fslist.getNodes();
+	    loadImages();
+	    JSONObject data = FSList.ArrayToJSONObject(nodes,screen.getLanguageCode(),"mask,url"); 
 			
-
-			String imageurl = model.getProperty("/screen/imageurl");
-			if (imageurl!=null && !imageurl.equals("")) {
-				data.put("url",imageurl);
-			} else {
-				data.put("url",stationnode.getProperty("url"));
-			}
+	    String imageurl = model.getProperty("/screen/imageurl");
+	    if (imageurl!=null && !imageurl.equals("")) {
+		data.put("url",imageurl);
+	    } else {
+		data.put("url",stationnode.getProperty("url"));
+	    }
 				
-			screen.get(selector).parsehtml(data);
-		}
-		model.onPropertiesUpdate("/shared['mupop']/station/"+model.getProperty("@stationid"),"onPositionChange",this);		
+	    screen.get(selector).parsehtml(data);
 	}
+	model.onPropertiesUpdate("/shared['mupop']/station/"+model.getProperty("@stationid"),"onPositionChange",this);		
+    }
 	
-	public void loadImages() {
-		for(Iterator<FsNode> iter = nodes.iterator() ; iter.hasNext(); ) {
-			FsNode node = (FsNode)iter.next();	
-			String mask = node.getProperty("mask");
-			try {
-				URL url = new URL(mask);		
-				BufferedImage nimg = ImageIO.read(url);
-				images.put(node.getId(), nimg);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}		
-	}
-	
-	
-	public void checkOverlays(float xp,float yp) {
-	    selecteditem = null;
-		for(Iterator<FsNode> iter = nodes.iterator() ; iter.hasNext(); ) {
-			FsNode node = (FsNode)iter.next();	
-			BufferedImage cimg = images.get(node.getId());
-			if (cimg!=null) {
-				try{	    	
-					int width = cimg.getWidth();
-					int height = cimg.getHeight();
-	        
-					float x= (width/100)*xp;
-					float y= (height/100)*yp;
-	        
-					int p = cimg.getRGB((int)x,(int)y);
-					int a = (p>>24) & 0xff;
-					int r = (p>>16) & 0xff;
-					int g = (p>>8) & 0xff;
-					int b = p & 0xff;
-					//System.out.println("X="+x+" Y="+y+"A="+a+" R="+r+" G="+g+" B="+b);
-					if (a>200 && g>100) {
-					    selecteditem = node;
-						screen.get("#zoomandaudio_layer"+node.getId()).css("opacity","0.3");
-					} else {
-						screen.get("#zoomandaudio_layer"+node.getId()).css("opacity","0");
-					}   
-				} catch(Exception e){
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    public void loadImages() {
+	for(Iterator<FsNode> iter = nodes.iterator() ; iter.hasNext(); ) {
+	    FsNode node = (FsNode)iter.next();	
+	    String mask = node.getProperty("mask");
+	    try {
+		URL url = new URL(mask);		
+		BufferedImage nimg = ImageIO.read(url);
+		images.put(node.getId(), nimg);
+	    } catch(Exception e) {
+		e.printStackTrace();
+	    }
+	}		
+    }
 
-	
-	public void onPositionChange(ModelEvent e) {
-		FsPropertySet set = (FsPropertySet)e.target;
-		
-		try {
-		 float x  = Float.parseFloat(set.getProperty("x"));
-		 float y  = Float.parseFloat(set.getProperty("y"));
-		
-		checkOverlays(x-1,y-1);
+    public void checkOverlays() {
+	//loop over every layer
+	for(Iterator<FsNode> iter = nodes.iterator() ; iter.hasNext(); ) {
+	    FsNode node = (FsNode)iter.next();	
+	    BufferedImage cimg = images.get(node.getId());
+	    if (cimg!=null) {
+		try {	    	
+		    int width = cimg.getWidth();
+		    int height = cimg.getHeight();
+		    
+		    boolean selected = false;
+		    Iterator it = spots.entrySet().iterator();
+		    //loop over every connected client to allow multi highlighting
+		    while (it.hasNext()) {
+			Map.Entry<String, HashMap<String, Double>> pair = (Map.Entry<String, HashMap<String, Double>>) it.next();
+			double xp = pair.getValue().get("x");
+			double yp = pair.getValue().get("y");
 			
-		 String action = set.getProperty("action");
-		 if (action.equals("move")) { // its a move event so lets just move the dot
-			 
-			//String url = getAudio(x, y);
-			//screen.get("#zoomandaudio_spot").css("background-color","purple");
-			// if (url!=null) {
-			//	screen.get("#zoomandaudio_spot").css("background-color","blue");
-			// }
-			screen.get("#zoomandaudio_spot").translate(""+x+"%",""+y+"%");
-		 } else if (action.equals("up")) {
-				//String url = getAudio(x, y);
-				 //if (url!=null) {
-			 	if (selecteditem!=null) {
-						FsNode message = new FsNode("message","1");
-						message.setProperty("action", "startaudio");
-						message.setProperty("url", selecteditem.getProperty("url"));
-						model.notify("/shared['mupop']/station/"+model.getProperty("@stationid"),message);
-				}
-		 }
+			//make sure to divide by a double, otherwise you will get a
+			// int value when dividing two ints
+			double x = (width/100.0)*xp;
+			double y = (height/100.0)*yp;
+        	        
+			int p = cimg.getRGB((int)x,(int)y);
+			int a = (p>>24) & 0xff;
+			int r = (p>>16) & 0xff;
+			int g = (p>>8) & 0xff;
+			int b = p & 0xff;
 
-		 
-		} catch(Exception error) {
-			System.out.println("PhotoInfoSpots - count not move stop of play sound");
-		}
-		
-	}
-	
-	/*
-	private String getAudio(float x,float y) {
-		FSList fslist = FSListManager.get("/domain/mecanex/app/sceneplayer/scene/blue/element/screen5/sounds",true);
-		List<FsNode> nodes = fslist.getNodes();
-		if (nodes!=null) {
-			for(Iterator<FsNode> iter = nodes.iterator() ; iter.hasNext(); ) {
-				FsNode node = (FsNode)iter.next();	
-				String url = node.getProperty("url");
-				try {
-					float ox = Float.parseFloat(node.getProperty("x"));
-					float oy = Float.parseFloat(node.getProperty("y"));
-					float ow = Float.parseFloat(node.getProperty("width"))/2; // 
-					float oh = Float.parseFloat(node.getProperty("height"))/2;
-					if (x>(ox-ow) && x<(ox+ow)) { // within x range
-						if (y>(oy-oh) && y<(oy+oh)) { // within y range
-							return url;
-						}
-					}	
-				} catch(Exception e) {
-					System.out.println("Error parsing audioimage data");
-				}
+			if (a>200 && g>100) {
+			    selecteditems.put(pair.getKey(), node);
+			    selected = true;
+			    break;
 			}
+		    }
+			
+		    if (selected) {
+    		    	screen.get("#zoomandaudio_layer"+node.getId()).css("opacity","0.3");
+		    } else {
+			screen.get("#zoomandaudio_layer"+node.getId()).css("opacity","0");
+		    }   
+		} catch(Exception e){
+		    //e.printStackTrace();
 		}
-		return null;
+	    }
 	}
-	*/
+    }
+
+    public void onPositionChange(ModelEvent e) {
+	FsPropertySet set = (FsPropertySet)e.target;
+		
+	try {
+	    double x  = Double.parseDouble(set.getProperty("x"));
+	    double y  = Double.parseDouble(set.getProperty("y"));
+	    String color = set.getProperty("color");
+			
+	    String action = set.getProperty("action");
+	    String deviceid = set.getProperty("deviceid");
+	    
+	    long currentTime = new Date().getTime();
+	    
+	    //check for a new spot
+	    if (!spots.containsKey(deviceid)) {
+		screen.get("#zoomandaudio_holder").append("<div class='zoomandaudio_spot' id='zoomandaudio_spot_"+deviceid+"'></div>");
+	    }
+	    
+	    //update last seen
+	    HashMap<String, Double> spot = new HashMap<String, Double>();
+	    spot.put("lastseen", (double) new Date().getTime());
+	    spot.put("x", x);
+	    spot.put("y", y);
+	    spots.put(deviceid, spot);
+	    
+	    //TODO: better to to this timeout based instead upon a move
+	    Iterator it = spots.entrySet().iterator();
+	    while (it.hasNext()) {
+		Map.Entry<String, HashMap<String, Double>> pair = (Map.Entry<String, HashMap<String, Double>>) it.next();
+		if (pair.getValue().get("lastseen") + 60000 < currentTime) {
+		    screen.get("#zoomandaudio_spot_"+pair.getKey()).remove();
+		    it.remove();
+		    selecteditems.remove(deviceid);
+		}
+	    }
+	    
+	    //check overlays after updating connected client list
+	    checkOverlays();
+	    
+	    if (action.equals("move")) { // its a move event so lets just move the dot
+		screen.get("#zoomandaudio_spot_"+deviceid).css("background-color", "#"+color);
+		screen.get("#zoomandaudio_spot_"+deviceid).translate(""+x+"%",""+y+"%");
+	    } else if (action.equals("up")) {
+		if (selecteditems.get(deviceid) != null) {
+		    System.out.println("Item is selected notify");
+		    FsNode message = new FsNode("message","1");
+		    message.setProperty("action", "startaudio");
+		    message.setProperty("url", selecteditems.get(deviceid).getProperty("url"));
+		    message.setProperty("deviceid", deviceid);
+		    model.notify("/shared['mupop']/station/"+model.getProperty("@stationid"),message);
+		}
+	    }
+	} catch(Exception error) {
+	    System.out.println("PhotoInfoSpots - count not move stop of play sound");
+	}	
+    }
 }
