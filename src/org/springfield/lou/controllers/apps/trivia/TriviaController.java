5bbd9a57e8da236ffd3078fd42b972b040d3be2d
Daniel Ockeloen <daniel@noterik.nl>
2017-05-03 11:55:57 +0200

    vacation sync

--- src/org/springfield/lou/controllers/apps/trivia/TriviaController.java
+++ src/org/springfield/lou/controllers/apps/trivia/TriviaController.java
@@ -40,11 +40,60 @@ import org.springfield.lou.model.ModelEvent;
 
 public class TriviaController extends Html5Controller {
 	public TriviaController() {}
+	private int timeout=0;
+
 
 	public void attach(String sel) {
+		model.setProperty("@contentrole","mainapp");
 		selector = sel;
-			JSONObject data = new JSONObject();
-			screen.get(selector).render(data);
+		fillPage();
+		model.onNotify("/shared[timers]/1second","on1SecondTimer",this); 
+	}
+	
+	private void fillPage() {
+		JSONObject data = new JSONObject();
+		FsNode item = model.getNode("@item");
+		if (item==null) {
+			item=getNextItem();
+		}
+		
+		String timeouts = item.getProperty("timeout");
+		timeout = 10;
+		try {
+			timeout = Integer.parseInt(timeouts);
+		} catch(Exception e) {
+		}
+		System.out.println("TIMEOUT="+timeout);
+		data.put("imageurl",item.getProperty("imageurl"));
+		screen.get(selector).render(data);
+		FsNode msgnode=new FsNode("msgnode","1");
+		msgnode.setProperty("itemid",model.getProperty("@itemid"));
+		model.notify("@appstate",msgnode);
+	}
+
+	public void on1SecondTimer(ModelEvent e) {
+		if (timeout>0) {
+			timeout = timeout -1;
+		} else {
+			getNextItem();
+			fillPage();
+		}
+		System.out.println("1 SEC TIMER "+timeout);
+	}
+	
+	private FsNode getNextItem() {
+		System.out.println("GET NEXT ITEM CALLED");
+		String selecteditem = model.getProperty("@selecteditem");
+		FsNode item = model.getNode("@item");
+		if (item==null) {
+			model.setProperty("@itemid","1");
+			item = model.getNode("@item");
+		} else {
+			// should pick a random one for now ill feed it number 2 !
+			model.setProperty("@itemid","2");
+			item = model.getNode("@item");
+		}
+		return item;
 	}
 
 
